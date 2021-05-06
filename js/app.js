495168a82ea050cd1c962c94a5cb4fa7d1d7b5a6
Ahmed Roble <airoble2019@gmail.com>
2021-05-06 14:43:26 -0400

    Add auto-complete to user search field & save settings in local storage

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 "use strict"
+
 // DOM selection
 const alertBanner = document.getElementById("alert")
 
@@ -7,8 +8,16 @@ const dailyCanvas = document.getElementById("daily-chart")
 const mobileCanvas = document.getElementById("mobile-chart")
 
 const user = document.getElementById("userField")
+const membersCollection = document.querySelectorAll(".member-name")
 const message = document.getElementById("messageField")
 const send = document.getElementById("sendBtn")
+
+const settings = document.getElementById("settings-login")
+const profile = document.getElementById("profile-visibility")
+const email = document.getElementById("email-notification")
+const timezones = document.getElementById("timezone")
+const save = document.getElementById("save")
+const cancel = document.getElementById("cancel")
 
 // alert banner
 alertBanner.innerHTML = `
@@ -146,8 +155,125 @@ let mobileChart = new Chart(mobileCanvas, {
   options: mobileOptions,
 })
 
-// message user
+// Auto-complete for user search field
+let names = []
+// create array of members names
+for (let i = 0; i < membersCollection.length; i++) {
+  names.push(membersCollection[i].textContent)
+}
 
+/*
+ * Autocomplete input field functionality with JS.
+ * This was adapted from an example from W3Schools
+ * Found here: https://www.w3schools.com/howto/howto_js_autocomplete.asp
+ 
+*/
+
+function autocomplete(inp, arr) {
+  /*the autocomplete function takes two arguments,
+  the text field element and an array of possible autocompleted values:*/
+  var currentFocus
+  /*execute a function when someone writes in the text field:*/
+  inp.addEventListener("input", function (e) {
+    var a,
+      b,
+      i,
+      val = this.value
+    /*close any already open lists of autocompleted values*/
+    closeAllLists()
+    if (!val) {
+      return false
+    }
+    currentFocus = -1
+    /*create a DIV element that will contain the items (values):*/
+    a = document.createElement("DIV")
+    a.setAttribute("id", this.id + "autocomplete-list")
+    a.setAttribute("class", "autocomplete-items")
+    /*append the DIV element as a child of the autocomplete container:*/
+    this.parentNode.appendChild(a)
+    /*for each item in the array...*/
+    for (i = 0; i < arr.length; i++) {
+      /*check if the item starts with the same letters as the text field value:*/
+      if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+        /*create a DIV element for each matching element:*/
+        b = document.createElement("DIV")
+        /*make the matching letters bold:*/
+        b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>"
+        b.innerHTML += arr[i].substr(val.length)
+        /*insert a input field that will hold the current array item's value:*/
+        b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>"
+        /*execute a function when someone clicks on the item value (DIV element):*/
+        b.addEventListener("click", function (e) {
+          /*insert the value for the autocomplete text field:*/
+          inp.value = this.getElementsByTagName("input")[0].value
+          /*close the list of autocompleted values,
+              (or any other open lists of autocompleted values:*/
+          closeAllLists()
+        })
+        a.appendChild(b)
+      }
+    }
+  })
+  /*execute a function presses a key on the keyboard:*/
+  inp.addEventListener("keydown", function (e) {
+    var x = document.getElementById(this.id + "autocomplete-list")
+    if (x) x = x.getElementsByTagName("div")
+    if (e.keyCode == 40) {
+      /*If the arrow DOWN key is pressed,
+        increase the currentFocus variable:*/
+      currentFocus++
+      /*and and make the current item more visible:*/
+      addActive(x)
+    } else if (e.keyCode == 38) {
+      //up
+      /*If the arrow UP key is pressed,
+        decrease the currentFocus variable:*/
+      currentFocus--
+      /*and and make the current item more visible:*/
+      addActive(x)
+    } else if (e.keyCode == 13) {
+      /*If the ENTER key is pressed, prevent the form from being submitted,*/
+      e.preventDefault()
+      if (currentFocus > -1) {
+        /*and simulate a click on the "active" item:*/
+        if (x) x[currentFocus].click()
+      }
+    }
+  })
+  function addActive(x) {
+    /*a function to classify an item as "active":*/
+    if (!x) return false
+    /*start by removing the "active" class on all items:*/
+    removeActive(x)
+    if (currentFocus >= x.length) currentFocus = 0
+    if (currentFocus < 0) currentFocus = x.length - 1
+    /*add class "autocomplete-active":*/
+    x[currentFocus].classList.add("autocomplete-active")
+  }
+  function removeActive(x) {
+    /*a function to remove the "active" class from all autocomplete items:*/
+    for (var i = 0; i < x.length; i++) {
+      x[i].classList.remove("autocomplete-active")
+    }
+  }
+  function closeAllLists(elmnt) {
+    /*close all autocomplete lists in the document,
+    except the one passed as an argument:*/
+    var x = document.getElementsByClassName("autocomplete-items")
+    for (var i = 0; i < x.length; i++) {
+      if (elmnt != x[i] && elmnt != inp) {
+        x[i].parentNode.removeChild(x[i])
+      }
+    }
+  }
+  /*execute a function when someone clicks in the document:*/
+  document.addEventListener("click", function (e) {
+    closeAllLists(e.target)
+  })
+}
+autocomplete(document.getElementById("userField"), names)
+
+// Validation on user inputs
 send.addEventListener("click", (e) => {
   // ensure user and message fields are filled out
   if (user.value === "" && message.value === "") {
@@ -160,4 +286,53 @@ send.addEventListener("click", (e) => {
     alert(`Message successfully sent to: ${user.value}`)
   }
   e.preventDefault()
+})
+
+// Add event listener to your Save button
+save.addEventListener("click", (e) => {
+  const currentTimeZone = document.getElementById("timezone")
+  // Inside that event listener, store all of the current values to local storage
+  if (email.checked) {
+    localStorage.setItem("email", "true")
+  } else {
+    localStorage.setItem("email", "false")
+  }
+  if (profile.checked) {
+    localStorage.setItem("profile", "true")
+  } else {
+    localStorage.setItem("profile", "false")
+  }
+  if (currentTimeZone.value) {
+    localStorage.setItem("selectedTimezone", currentTimeZone.value)
+  }
+})
+
+// Load the stored values
+function loadSettings() {
+  if (localStorage.getItem("email") === "true") {
+    email.checked = true
+  } else {
+    email.checked = false
+  }
+  if (localStorage.getItem("profile") === "true") {
+    profile.checked = true
+  } else {
+    profile.checked = false
+  }
+  if (localStorage.getItem("selectedTimezone")) {
+    timezone.value = localStorage.getItem("selectedTimezone")
+  }
+}
+// Call load function on every page load
+
+window.onload = function () {
+  loadSettings()
+}
+
+// Add event listener to Cancel button
+cancel.addEventListener("click", (e) => {
+  localStorage.clear()
+  email.checked = false
+  profile.checked = false
+  timezone.value = ""
 })
